9be242ed81e7e3cd43f5961aec5f315539fcb449
elouadi abdelati <Elouadi_Abdelati@outlook.fr>
2021-01-16 20:36:56 +0100

    add scripts shell

---
+++ resources/script/modify_lodg_mode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ORACLE_BASE=/u01/app/oracle; export ORACLE_BASE;
+ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe; export ORACLE_HOME;
+NLS_LANG=AMERICAN_AMERICA.AL32UTF8; export NLS_LANG;
+ORACLE_SID=XE; export ORACLE_SID;
+
+$ORACLE_HOME/bin/sqlplus -s  $1/$2  <<EOF
+ Shutdown
+ Startup mount
+ Alter database $3;
+ alter database open;
+EOF
