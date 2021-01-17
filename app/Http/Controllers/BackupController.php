dff7b72714307346a8af9f5218c2a6032aab8f36
elouadi abdelati <Elouadi_Abdelati@outlook.fr>
2021-01-17 23:23:05 +0100

    add .env

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -9,72 +9,72 @@ use Illuminate\Http\Request;
 class BackupController extends Controller
 {
 
-   public function test(){
+    public function test()
+    {
         /**
          * good
          */
-    //  $rs = Backup::listBuckups("sys","elouadi");
+    $rs = Backup::listBuckups("sys","elouadi");
 
-      /**
+        /**
          * good
          */
-    //   $rs = Backup::fullBackup("sys","elouadi");
+        //   $rs = Backup::fullBackup("sys","elouadi");
 
 
-       /**
-        * good
-        */
-      // $rs = Database::checkCurrentScn("system","elouadi");
+        /**
+         * good
+         */
+        // $rs = Database::checkCurrentScn("system","elouadi");
 
-      /**
-       * working
-       */
-     // $rs = Database::modifyLogMode("system","elouadi","archivelog");
+        /**
+         * working
+         */
+        // $rs = Database::modifyLogMode("system","elouadi","archivelog");
 
-     /**
-      * good
-      */
-     $rs = Database::logMode("sys","elouadi");
+        /**
+         * good
+         */
+       // $rs = Database::logMode("sys", "elouadi");
 
-     /**
-      * Working
-      *RMAN utilise le SCN sélectionné comme base pour cette sauvegarde incrémentielle.
-      *Pour tous les fichiers en cours de sauvegarde, RMAN inclut tous les blocs de données
-      *qui ont été modifiés à des SCN supérieurs ou égaux à FROM SCNdans la sauvegarde
-      * incrémentielle.
-      */
+        /**
+         * Working
+         *RMAN utilise le SCN sélectionné comme base pour cette sauvegarde incrémentielle.
+         *Pour tous les fichiers en cours de sauvegarde, RMAN inclut tous les blocs de données
+         *qui ont été modifiés à des SCN supérieurs ou égaux à FROM SCNdans la sauvegarde
+         * incrémentielle.
+         */
 
-     // $rs = Backup::backupIncremental("sys","elouadi","807334");
-
-
-    /**
-      * WORKING
-      */
-      //$rs = Backup::differentialBackup("sys","elouadi","1","JEU_INC_N1_DIF");
-
-    /**
-      * WORKING
-      */
-      //$rs = Backup::cumulativeBackup("sys","elouadi","1","JEU_INC_N1_CUM");
-
-    /**
-      * WORKING
-      */
-     // $rs = Backup::listBackupsetTag("sys","elouadi","JEU_INC_N1_CUM");
+        // $rs = Backup::backupIncremental("sys","elouadi","807334");
 
 
+        /**
+         * WORKING
+         */
+        //$rs = Backup::differentialBackup("sys","elouadi","1","JEU_INC_N1_DIF");
 
-    /**
-      * WORKING
-      */
-      // $rs = Backup::deteleAll("sys","elouadi");
+        /**
+         * WORKING
+         */
+        //$rs = Backup::cumulativeBackup("sys","elouadi","1","JEU_INC_N1_CUM");
+
+        /**
+         * WORKING
+         */
+        // $rs = Backup::listBackupsetTag("sys","elouadi","JEU_INC_N1_CUM");
 
 
-    /**
-      * WORKING
-      */
-     // $rs = Backup::deteleByNumber("sys","elouadi","3");
-       return view('backup',['listBackup' =>  $rs ]);
 
-   }
+        /**
+         * WORKING
+         */
+        // $rs = Backup::deteleAll("sys","elouadi");
+
+
+        /**
+         * WORKING
+         */
+        // $rs = Backup::deteleByNumber("sys","elouadi","3");
+        return view('backup', ['listBackup' =>  $rs]);
+    }
 }
